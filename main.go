b47de7518b4ec49aa702f0bc8c9edd24cd77b587
matthewmu <matty.mu96@gmail.com>
2024-01-21 17:00:08 +1100

    added more extensible reverse function

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func findFirstDigit(line string) (int, error) {
 			return int(char - '0'), nil
 		}
 	}
-	return 0, fmt.Errorf("no digit found in line: ", line)
+	return 0, fmt.Errorf("no digit found in line: %s", line)
 }
 func reverseString(line string) string {
 	runes := []rune(line)
@@ -24,16 +24,19 @@ func reverseString(line string) string {
 
 func findLastDigit(line string) (int, error) {
 
-	revLine := reverseString(line)
-	for _, char := range revLine {
+	//revLine := reverseString(line)
+	/*	for _, char := range revLine {
+		fmt.Print(string(char))
 		if char >= '0' && char <= '9' {
 			return int(char - '0'), nil
 		}
-	}
+	}*/
 	for i := len(line) - 1; i >= 0; i-- {
-
+		if line[i] >= '0' && line[i] <= '9' {
+			return int(line[i] - '0'), nil
+		}
 	}
-	return 0, fmt.Errorf("no digit found in line: ", line)
+	return 0, fmt.Errorf("no digit found in line: %s", line)
 }
 
 func process(gioename string) (int, error) {
@@ -57,6 +60,8 @@ func process(gioename string) (int, error) {
 			fmt.Println(err)
 		}
 
+		lineAns := result * 10
+		fmt.Printf("\nfirst: %d Second: %d\n", lineAns, rev)
 		answer += result*10 + rev
 	}
 	if err := scanner.Err(); err != nil {
